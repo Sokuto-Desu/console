c93b90808903b7575292478dc9f1d520e05cb0d2
Sokuto Desu <sokutodesu@gmail.com>
2023-02-15 20:47:22 +0600

    ac

--- cogs/owner.py
+++ cogs/owner.py
@@ -14,9 +14,9 @@ class Owner(commands.Cog):
 		return self.bot.is_owner(ctx.author)
 	
 	
-	@slash_command(guild_ids=config["devserver"])
-	async def run(self, ctx, c: Option(str, required=True, options=["eval", "exec"]), *, data: Option(str, required=True)):
-		if c.startswith("ev"):
+	@slash_command(guild_ids = config["devserver"])
+	async def run(self, ctx, c: Option(str, required = True, choices = ["eval", "exec"]), *, data: Option(str, required = True)):
+		if c = "eval:
 			try:
 				r = eval(data)
 			except:
@@ -24,16 +24,16 @@ class Owner(commands.Cog):
 			
 			await ctx.respond(f"```py\n{r}```")
 		
-		elif c.startswith("ex"):
+		elif c == "exec:
 			data = "\n" + data if not data.startswith("\n") else data
 			tabed_code = "  ".join(data.split("\n")).replace("  ", "\n  ")
 			
 			clcb = asyncio.get_running_loop()
 			
-			return exec(f"async def __ex():\n	try:\n{tabed_code}\n	except Exception as e:\n		await ctx.send(str(e)[0:1995])\nclcb.create_task(__ex())", globals().update({"bot": bot, "ctx": ctx}))
+			return exec(f"async def __ex():\n	try:\n{tabed_code}\n	except Exception as e:\n		await ctx.respond(str(e)[0:1995])\nclcb.create_task(__ex())", globals().update({"bot": bot, "ctx": ctx}))
 	
 	
-	@slash_command(guild_ids=config["devserver"])
+	@slash_command(guild_ids = config["devserver"])
 	async def shutdown(self, ctx):
 		await ctx.respond("`closing connection...`")
 		await bot.close()
