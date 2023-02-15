92faaf6f1f7d4f0c001907a21d3ce631c39e713c
Sokuto Desu <sokutodesu@gmail.com>
2023-02-15 20:07:29 +0600

    ac

--- core/bot.py
+++ core/bot.py
@@ -1,38 +1,37 @@
 import discord
-from discord.ext import commands
 
 import settings
 from .utils import handle_error
 
 
-class ConsoleBot(commands.Bot):
-	def __init__(self):
-		super().__init__(
-		command_prefix = settings.config["prefix"],
-		intents = discord.Intents.all(),
-		case_insensitive = True,
-		activity = discord.Activity(
-			type = discord.ActivityType.watching,
-			name = settings.config["activity"]
-			)
-		)
-		
-		self.is_test = settings.config["is_test"]
-		self.cogs = settings.cogs
-		
-		for cog in self.cogs:
-			self.load_extension(cog)
-	
-	async def on_application_command_error(self, ctx, error):
-		await handle_error(ctx, error)
-	
-	
-	async def on_ready(self):
-		print("-"*25)
-		print(f"Logged in as {self.user} (test mode: {self.is_test.lower()})")
-		print("-"*25)
-	
-	def run(self):
-		if self.is_test:
-			return super().run(settings.config["token"])
-		super().run(settings.config["test_token"])
+class ConsoleBot(discord.Bot):
+ 	def __init__(self):
+ 		super().__init__(
+#		command_prefix = settings.config["prefix"],
+ 		intents = discord.Intents.all(),
+ 		case_insensitive = True,
+ 		activity = discord.Activity(
+ 			type = discord.ActivityType.watching,
+ 			name = settings.config["activity"]
+ 			)
+ 		)
+ 		
+ 		self.is_test = settings.config["is_test"]
+ 		self.cogs = settings.cogs
+ 		
+ 		for cog in self.cogs:
+ 			self.load_extension(cog)
+ 	
+ 	async def on_application_command_error(self, ctx, error):
+ 		await handle_error(ctx, error)
+ 	
+ 	
+ 	async def on_ready(self):
+ 		print("-"*25)
+ 		print(f"Logged in as {self.user} (test mode: {self.is_test.lower()})")
+ 		print("-"*25)
+ 	
+ 	def run(self):
+ 		if self.is_test:
+ 			return super().run(settings.config["token"])
+ 		super().run(settings.config["test_token"])
