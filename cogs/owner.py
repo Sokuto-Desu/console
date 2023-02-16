080a0a258aea9ca316a068fc852504c7097cac61
Sokuto Desu <sokutodesu@gmail.com>
2023-02-16 20:15:42 +0600

    ac

--- cogs/owner.py
+++ cogs/owner.py
@@ -19,11 +19,11 @@ class Owner(commands.Cog):
 	async def run(self, ctx, mode: str, *, data: str):
 		if mode == "eval":
 			try:
-				r = eval(data)
+				result = eval(data)
 			except:
-				r = await eval(data)
+				result = await eval(data)
 			
-			await ctx.respond(f"```py\n{r}```")
+			await ctx.respond(f"```py\n{result}```")
 		
 		elif mode == "exec":
 			data = "\n" + data if not data.startswith("\n") else data
@@ -31,13 +31,13 @@ class Owner(commands.Cog):
 			
 			_running_loop = asyncio.get_running_loop()
 			
-			return exec(f"async def __ex():\n	try:\n{tabed_code}\n	except Exception as e:\n		await ctx.respond(str(e)[0:1995])\n_running_loop.create_task(__ex())", globals().update({"bot": bot, "ctx": ctx}))
+			return exec(f"async def __ex():\n	try:\n{tabed_code}\n	except Exception as e:\n		await ctx.respond(str(e)[0:1995])\n_running_loop.create_task(__ex())", globals().update({"bot": self.bot, "ctx": ctx}))
 	
 	
 	@slash_command(guild_ids = [settings.devserver])
 	async def shutdown(self, ctx):
 		await ctx.respond("`closing connection...`")
-		await bot.close()
+		await self.bot.close()
 
 
 def setup(bot):
