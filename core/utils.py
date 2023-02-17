f57487826891c9ce62fe13df06f75363d2d53e6c
Sokuto Desu <sokutodesu@gmail.com>
2023-02-17 14:07:40 +0600

    ac

--- core/utils.py
+++ core/utils.py
@@ -1,9 +1,12 @@
-import settings
-import discord
+from asyncio import sleep
+
+from discord import Embed as DiscordEmbed
+from discord import Message
 
 
-async def close(message: discord.Message, time: int = 5):
-	await asyncio.sleep(time)
+
+async def close(message: Message, time: int = 5):
+	await sleep(time)
 	await message.delete()
 
 # converts time (8m, 1h, 2d, etc.) to seconds
@@ -21,7 +24,7 @@ async def convert_time(time):
 	return times
 
 
-class Embed(discord.Embed):
+class Embed(DiscordEmbed):
 	@staticmethod
 	def create(**parameters):
 		if not parameters.get("color"):
@@ -40,6 +43,9 @@ class Embed(discord.Embed):
 		
 		for parameter in ("image", "thumbnail", "footer", "title", "description"):
 			if parameters.get(parameter) in (None, Embed.Empty):
-				parameters.pop(parameter)
+				try:
+					parameters.pop(parameter)
+				except KeyError:
+					pass
 		
-		return discord.Embed.from_dict(parameters)
+		return DiscordEmbed.from_dict(parameters)
